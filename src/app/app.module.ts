70fd7ba612c3c2fb4064509d7cd41b5894cc69a1
Alejandro Bajo <alebajodiaz@gmail.com>
2021-11-16 13:13:24 +0100

    Creamos la lista con espacios

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialImportsModule } from './modules/material-imports.module';
+import { MoviesPopularListComponent } from './components/movies-popular-list/movies-popular-list.component';
+import { MovieItemComponent } from './components/movie-item/movie-item.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MoviesPopularListComponent,
+    MovieItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialImportsModule,
+    HttpClientModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
